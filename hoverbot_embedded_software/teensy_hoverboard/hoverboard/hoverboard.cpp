9fb6dd366d9bb0fbcbb5ba7c65de1ddae911dfdd
remi <remi.pincent@inria.fr>
2016-11-30 11:13:07 +0100

    Add control methods in Hoverboard class

--- hoverbot_embedded_software/teensy_hoverboard/hoverboard/hoverboard.cpp
+++ hoverbot_embedded_software/teensy_hoverboard/hoverboard/hoverboard.cpp
@@ -36,7 +36,6 @@
  **************************************************************************/
 #include "hoverboard.h"
 
-#include <Arduino.h>
 #include <IntervalTimer.h>
 
 /**************************************************************************
@@ -51,6 +50,11 @@
  * Static members definitions
  **************************************************************************/
 const uint8_t Hoverboard::SHORT_PRESS_DUR_MS = 100;
+const uint8_t Hoverboard::GYRO_FRAME_LENGTH = 6;
+const uint8_t Hoverboard::GYRO_CONTACT_CLOSED_BYTE = 85;
+const uint8_t Hoverboard::GYRO_CONTACT_OPENED_BYTE = 170;
+const uint16_t Hoverboard::GYRO_FRAME_START = 256;
+
 Hoverboard* Hoverboard::_instance = NULL;
 
 /**************************************************************************
@@ -61,15 +65,27 @@ Hoverboard::Hoverboard(const Hoverboard::Config& arg_config):
   _hallSensor2(arg_config._motor2Conf),
   _u16_powerPin(arg_config._u16_powerPin),
   _timer(),
-  _e_state(POWER_OFF)
+  _e_state(POWER_OFF),
+	_s16_calValue(0),
+	_p_gyro1Serial(arg_config._p_gyro1Serial),
+	_p_gyro2Serial (arg_config._p_gyro2Serial),
+  _f_speed1(0.0),
+  _f_speed2(0.0),
+  _f_rampUpFactor(0.3)
 {
-  pinMode(_u16_powerPin, OUTPUT);
-  powerOff(); 
-  
   _instance = this;
 }  
   
+Hoverboard::EHoverboardErr Hoverboard::init(void)
+{
+	 _p_gyro1Serial->begin (26300, SERIAL_9N1);  // 9 bits mode
+   _p_gyro2Serial->begin (26300, SERIAL_9N1);  // 9 bits mode
 
+  pinMode(_u16_powerPin, OUTPUT);
+  powerOff(); 
+
+	return NO_ERROR;
+}
   
 Hoverboard::EHoverboardErr Hoverboard::powerOn(void)
 {
@@ -106,10 +122,10 @@ Hoverboard::EHoverboardErr Hoverboard::powerOff(void)
 
 Hoverboard::EHoverboardErr Hoverboard::powerOffAsync(void)
 {
-   digitalWrite(_u16_powerPin, LOW);
   //TODO check status 
   if(_e_state != POWER_OFF || _e_state != POWERING_OFF)
   {
+		Serial.println("powering off");
     _e_state = POWERING_OFF;
     digitalWrite(_u16_powerPin, HIGH);
     _timer.begin(Hoverboard::timerIt, Hoverboard::SHORT_PRESS_DUR_MS*1000); 
@@ -117,18 +133,96 @@ Hoverboard::EHoverboardErr Hoverboard::powerOffAsync(void)
   return NO_ERROR; 
 }
 
+Hoverboard::EHoverboardErr Hoverboard::setSpeed(float arg_f_speed1, float arg_f_speed2)
+{
+  /** Are both motors rotating in same direction ? */
+  if((arg_f_speed1 >= 0 && arg_f_speed2 >= 0) || (arg_f_speed1 < 0 && arg_f_speed2 < 0)) 
+  {
+    setSpeedSameRota(arg_f_speed1, arg_f_speed2); 
+  } 
+  else /** Motors do not rotate in same direction */
+  {
+  }
+  _f_speed1 = arg_f_speed1; 
+  _f_speed2 = arg_f_speed2; 
+} 
+    
 Hoverboard::EHoverboardErr Hoverboard::stop(void)
 {
-  /** Send a frame indicating user is not on hoverboard */
-  
 }
 
+void Hoverboard::idleControl(void)
+{
+  /** Idle control => apply calibration value when motors rotate same direction */
+  simulateGyro1(_s16_calValue);
+  simulateGyro2(-_s16_calValue);
+}
 /**************************************************************************
  * Private Methods Definitions
  **************************************************************************/
 Hoverboard::EHoverboardState Hoverboard::getState(void)
 {
 }
+
+void Hoverboard::simulateGyro1(int16_t arg_s16_target)
+{
+	/** send a frame simulating gyroscope move on gyro 
+	 * daughterboard 1 */
+	uint16_t loc_au16_gyroFrame[GYRO_FRAME_LENGTH] = {
+		GYRO_FRAME_START, 
+		(uint8_t)(arg_s16_target & 0xff), 
+		(uint8_t)(arg_s16_target >> 8 & 0xff), 
+		(uint8_t)(arg_s16_target & 0xff), 
+		(uint8_t)(arg_s16_target >> 8 & 0xff), 
+		GYRO_CONTACT_CLOSED_BYTE};
+
+	for(uint8_t index = 0; index < 6; index++)
+	{
+		_p_gyro1Serial->write9bit(loc_au16_gyroFrame[index]); 
+	} 
+}  
+    
+void Hoverboard::simulateGyro2(int16_t arg_s16_target)
+{
+  /** send a frame simulating gyroscope move on gyro 
+   * daughterboard 2 */
+  uint16_t loc_au16_gyroFrame[GYRO_FRAME_LENGTH] = {
+    GYRO_FRAME_START, 
+    (uint8_t)(arg_s16_target & 0xff), 
+    (uint8_t)(arg_s16_target >> 8 & 0xff), 
+    (uint8_t)(arg_s16_target & 0xff), 
+    (uint8_t)(arg_s16_target >> 8 & 0xff), 
+    GYRO_CONTACT_CLOSED_BYTE};
+  
+  for(uint8_t index = 0; index < 6; index++)
+  {
+    _p_gyro2Serial->write9bit(loc_au16_gyroFrame[index]);
+  }
+}
+
+/**
+* When motors rotate in same direction. Hoverboard performs an integral
+* control from daugtherboards gyroscope data. In order to keep a clean 
+* control, we must simulate an integral control on simulated gyros/
+*/ 
+void Hoverboard::setSpeedSameRota(float arg_f_speed1, float arg_f_speed2)
+{
+  int16_t  loc_s16_speedRampUp1 = (arg_f_speed1 > 0) ? (arg_f_speed1 - _f_speed1)*_f_rampUpFactor : -(arg_f_speed1 - _f_speed1)*_f_rampUpFactor;      
+  int16_t  loc_s16_speedRampUp2 = (arg_f_speed2 > 0) ? (arg_f_speed2 - _f_speed2)*_f_rampUpFactor : -(arg_f_speed2 - _f_speed2)*_f_rampUpFactor;      
+
+  Serial.println(arg_f_speed1);
+  Serial.println(arg_f_speed2);
+  Serial.println(loc_s16_speedRampUp1);
+  Serial.println(loc_s16_speedRampUp2);
+  uint32_t loc_u32_timeStart = millis();
+  while((millis() - loc_u32_timeStart) < (1/_f_rampUpFactor)*1000)
+  {
+    /** Same rota direction => refer README.md - Gyro simualated target must from different
+     * signum */
+    simulateGyro1(loc_s16_speedRampUp1);
+    simulateGyro2(-loc_s16_speedRampUp2);
+  }
+} 
 
 void Hoverboard::timerIt(void)
 {
@@ -144,4 +238,5 @@ void Hoverboard::timerIt(void)
   }
   _instance->_timer.end();
 }
+
 
--- hoverbot_embedded_software/teensy_hoverboard/hoverboard/hoverboard.h
+++ hoverbot_embedded_software/teensy_hoverboard/hoverboard/hoverboard.h
@@ -27,6 +27,7 @@
  * Include Files
  **************************************************************************/
 #include <stdint.h>
+#include <Arduino.h>
 #include <brushless_hall_sensor.h>
 
 /**************************************************************************
@@ -71,12 +72,19 @@ class Hoverboard
         uint16_t _u16_powerPin;
         BrushlessHallSensor::Config _motor1Conf;
         BrushlessHallSensor::Config _motor2Conf;
+				HardwareSerial * _p_gyro1Serial, * _p_gyro2Serial;				
+
         inline Config(const uint16_t arg_u16_powerPin,
             BrushlessHallSensor::Config& arg_motor1HallSensorConf,
-            BrushlessHallSensor::Config& arg_motor2HallSensorConf):
+            BrushlessHallSensor::Config& arg_motor2HallSensorConf,
+						HardwareSerial& arg_gyro1Serial,
+						HardwareSerial& arg_gyro2Serial):
           _u16_powerPin(arg_u16_powerPin),
           _motor1Conf(arg_motor1HallSensorConf),
-          _motor2Conf(arg_motor2HallSensorConf){};
+          _motor2Conf(arg_motor2HallSensorConf),
+					_p_gyro1Serial(&arg_gyro1Serial),
+					_p_gyro2Serial(&arg_gyro2Serial)
+				{};
     };
 
     /** private members */  
@@ -92,17 +100,35 @@ class Hoverboard
      **/
     IntervalTimer _timer;
     volatile EHoverboardState _e_state;
+    int16_t _s16_calValue;
+		HardwareSerial * _p_gyro1Serial, * _p_gyro2Serial;				
+    float _f_speed1, _f_speed2;
+    /** Factor to reach target speed - time to reach speed = 1/ramp_up_factor */ 
+    float _f_rampUpFactor;
+    
     static Hoverboard* _instance; 
     
     static const uint8_t SHORT_PRESS_DUR_MS;
+		/** Gyro daughterboard frame length : depends on hoverboard firmware */
+		static const uint8_t GYRO_FRAME_LENGTH;
+		/** Byte sent in gyro frame indicating user has its feet on gyro board */
+		static const uint8_t GYRO_CONTACT_CLOSED_BYTE;
+		/** Byte sent in gyro frame indicating user does not have its feet on gyro board */
+		static const uint8_t GYRO_CONTACT_OPENED_BYTE;
+		static const uint16_t GYRO_FRAME_START;
 
     /** public methods */
   public :
     Hoverboard(const Hoverboard::Config&);
+		EHoverboardErr init(void);
     EHoverboardErr powerOn(void);
     EHoverboardErr powerOnAsync(void);
     EHoverboardErr powerOff(void);
     EHoverboardErr powerOffAsync(void);
+
+    /** Set speed for motors  */ 
+    EHoverboardErr setSpeed(float arg_f_speed1, float arg_f_speed2);
+    
     /**
      * Stop any control 
      **/
@@ -113,9 +139,16 @@ class Hoverboard
      * will enter error mode */
     void idleControl(void); 
 
+    EHoverboardErr calibrate(void);
+
+		/** TODO get target range  - make it private*/
+    void simulateGyro1(int16_t arg_s16_target);
+    void simulateGyro2(int16_t arg_s16_target);
+
     /** private methods */  
   private :
     EHoverboardState getState(void);
+    void setSpeedSameRota(float arg_f_speed1, float arg_f_speed2);
     static void timerIt(void);
 };
 
